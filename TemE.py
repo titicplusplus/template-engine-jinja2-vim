636f62f802e2804869e586695c589819262230c5
titicplusplus <77501831+titicplusplus@users.noreply.github.com>
2021-06-09 21:48:40 +0200

    update TemE.py

--- TemE.py
+++ TemE.py
@@ -1,7 +1,7 @@
 from jinja2 import Template
 import sys
 
-print( sys.argv[:] )
+#print( sys.argv[:] )
 
 if len(sys.argv) > 2:
     pos = sys.argv[1].find("jinja2.")
@@ -17,3 +17,20 @@ if len(sys.argv) > 2:
     text_file = open(file_N, "w")
     text_file.write(template.render())
     text_file.close()
+elif len(sys.argv) == 2:
+    pos = sys.argv[1].find("jinja2.")
+
+    if pos == -1:
+        sys.exit()
+
+    file_N = sys.argv[1][0:pos] + sys.argv[1][pos+7::]
+
+    with open(sys.argv[1], 'r') as file:
+        data = file.read()
+
+    template = Template(data,  trim_blocks=True)
+
+    text_file = open(file_N, "w")
+    text_file.write(template.render())
+    text_file.close()
+
